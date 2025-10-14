1e92c16ddbf54ff98b7a379e937fdb02ac0817d3
jiya-codes-cs <jiyabangar.official@gmail.com>
2025-10-14 09:44:33 -0600

    Made the grid using arrays

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -5,11 +5,53 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let rows = 8;
+let columns = 8;
+let cellSize = 50;
+
+// just basic colors for now (maybe change to images later?)
+let candyTypes = ["red", "blue", "green", "yellow", "purple"];
+
+class Candy {
+  constructor(type, row, column) {
+    this.type = type;
+    this.row = row;
+    this.column = column;
+  }
+
+  draw() {
+    fill(this.type);
+    circle(this.column * cellSize + cellSize/2, this.row * cellSize + cellSize/2, cellSize * 0.8);
+  }
+}
+
+let board = []; // 2D array for the board
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(columns * cellSize, rows * cellSize);
+  makeBoard();
 }
 
 function draw() {
-  background(220);
+  background(200);
+  drawBoard();
 }
+
+function makeBoard() {
+  for (let r = 0; r < rows; r++) {
+    board[r] = [];
+    for (let c = 0; c < columns; c++) {
+      let randomType = random(candyTypes);
+      board[r][c] = new Candy(randomType, r, c);
+    }
+  }
+}
+
+function drawBoard() {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      board[r][c].draw();
+    }
+  }
+}
+
