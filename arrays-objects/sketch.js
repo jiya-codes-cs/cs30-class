3a7b98735be439c9c6cd05a751a4c47c45ae9b37
jiya-codes-cs <jiyabangar.official@gmail.com>
2025-10-20 09:52:25 -0600

    added more functions to make candy swap better

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -9,29 +9,31 @@
 // - Board (done)
 // - Candies (done)
 // - Setting it to the middle of the screen
-// - Candy position
+// - Candy position (in progress)
 // - Add a Timer
 // - Add a Start Screen
 // - Add messages
 
 let rows = 8;
-let cols = 8;
+let columns = 8;
 let cellSize = 50;
 
 let candyColors = ["red", "blue", "green", "yellow", "purple"];
 
 class Candy {
-  constructor(color, row, col) {
+  constructor(color, row, column) {
     this.color = color;
     this.row = row;
-    this.col = col;
+    this.column = column;
   }
 
   draw() {
-    fill(this.color);
-    stroke(0);
-    strokeWeight(2);
-    circle(this.col * cellSize + cellSize/2, this.row * cellSize + cellSize/2, cellSize * 0.8);
+    if (!this.color) {
+      fill(this.color);
+      stroke(0);
+      strokeWeight(2);
+      circle(this.column * cellSize + cellSize/2, this.row * cellSize + cellSize/2, cellSize * 0.8); 
+    }
   }
 }
 
@@ -39,28 +41,31 @@ let board = [];
 let selectedCandy = null;
 
 function setup() {
-  createCanvas(cols * cellSize, rows * cellSize);
-  initBoard();
+  createCanvas(columns * cellSize, rows * cellSize);
+  makeBoard();
 }
 
 function draw() {
   background(220);
   drawBoard();
-  removeMatches();
-  applyGravity();
+
+  // continuously checks for matches and applies gravity
+  if (removeMatches()) {
+    applyGravity();
+  }
 }
 
 
 // created random color candy's that will be displayed on the board
-function initBoard() {
+function makeBoard() {
   for (let r = 0; r < rows; r++) {
     board[r] = [];
-    for (let c = 0; c < cols; c++) {
+    for (let c = 0; c < columns; c++) {
       let newColor;
       do {
         newColor = random(candyColors);
-      } while ((c >= 2 && board[r][c-1].color === newColor && board[r][c-2].color === newColor) ||
-               (r >= 2 && board[r-1][c].color === newColor && board[r-2][c].color === newColor));
+      } while (c >= 2 && board[r][c-1].color === newColor && board[r][c-2].color === newColor ||
+               r >= 2 && board[r-1][c].color === newColor && board[r-2][c].color === newColor);
       board[r][c] = new Candy(newColor, r, c);
     }
   }
@@ -68,14 +73,14 @@ function initBoard() {
 // created a function that makes the board on which the game will run
 function drawBoard() {
   for (let r = 0; r < rows; r++) {         
-    for (let c = 0; c < cols; c++) {
+    for (let c = 0; c < columns; c++) {
       board[r][c].draw();
       // highlight selected candy
-      if (selectedCandy && selectedCandy.row === r && selectedCandy.col === c) {
+      if (selectedCandy && selectedCandy.row === r && selectedCandy.column === c) {
         strokeWeight(4);
         stroke(255, 255, 0);
         noFill();
-        rect(c*cellSize+2, r*cellSize+2, cellSize-4, cellSize-4);
+        rect(c * cellSize + 2, r * cellSize + 2, cellSize - 4, cellSize - 4);
       }
     }
   }
@@ -85,68 +90,100 @@ function drawBoard() {
 function mousePressed() {
   let c = floor(mouseX / cellSize);
   let r = floor(mouseY / cellSize);
-  if (r >= 0 && r < rows && c >= 0 && c < cols) {
+  if (r >= 0 && r < rows && c >= 0 && c < columns) {
     if (!selectedCandy) {
       selectedCandy = board[r][c]; // select first candy
-    } else {
+    } 
+    else {
       // try to swap if neighbor
       if (isNeighbor(selectedCandy, board[r][c])) {
         swapCandies(selectedCandy, board[r][c]);
         selectedCandy = null;
-      } else {
-        selectedCandy = board[r][c]; // change selection (swap done)
+
+      // only swap if it creates a match
+      if (!hasMatch()) swapCandies(selectedCandy, board[r][c]); {
+        selectedCandy = null;
       }
+    } 
+    else {
+      selectedCandy = board[r][c]; // change selection (swap done)
     }
   }
 }
 
+
 // this function checks if 2 candies are neighbors
-function isNeighbor(c1, c2) {
-  return (abs(c1.row - c2.row) + abs(c1.col - c2.col)) === 1;
+function isNeighbor(a, b) {
+  return abs(a.row - b.row) + abs(a.column - b.column) === 1;
 }
 
 // this funcion swaps 2 candies
 function swapCandies(c1, c2) {
-  let tempColor = c1.color;
-  c1.color = c2.color;
-  c2.color = tempColor;
+  let tempColor = a.color;
+  a.color = b.color;
+  b.color = tempColor;
+}
+
+// function checks if any matches exist
+function hasMatch() {
+  return removeMatches(true);
 }
 
 // remove matches (3 or more in a row or column)
 function removeMatches() {
-  let toRemove = [];
-  // horizontal check
+  let matched = [];
+
+  // horizontal matches
   for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < cols - 2; c++) {
-      if (board[r][c].color === board[r][c+1].color && board[r][c].color === board[r][c+2].color) {
-        toRemove.push([r,c],[r,c+1],[r,c+2]);
+    for (let c = 0; c < columns - 2; c++) {
+      let color = board[r][c].color;
+      if(!color) continue;
+      if (board[r][c + 1].color === color && board[r][c + 2].color === color) {
+        let k = c;
+        while(k < columns && board[r][k].color === color) {
+          matched.push([r],[k]);
+          k++;
+        }
+        c = k - 1;
       }
     }
   }
-  // vertical check
-  for (let c = 0; c < cols; c++) {
+  // vertical matches
+  for (let c = 0; c < columns; c++) {
     for (let r = 0; r < rows - 2; r++) {
-      if (board[r][c].color === board[r+1][c].color && board[r][c].color === board[r+2][c].color) {
-        toRemove.push([r,c],[r+1,c],[r+2,c]);
+      let color = board[r][c].color;
+      if(!color) continue;
+      if (board[r + 1][c].color === color && board[r + 2][c].color === color) {
+        let k = r;
+        while(k < rows && board[r][k].color === color) {
+          matched.push([k],[c]);
+          k++;
+        }
+        r = k - 1;
       }
     }
   }
 
-// removes canies by setting color to null
+  if (testOnly) return matched.length > 0; 
+
+  // removes canies by setting color to null
   for (let [r,c] of toRemove) {
     board[r][c].color = null;
   }
+  return matched.length > 0;
 }
 
-// applies gravity and fills emppty slots
+// applies gravity and fills empty slots
 function applyGravity() {
-  for (let c = 0; c < cols; c++) {
-    for (let r = rows-1; r >= 0; r--) {
+  for (let c = 0; c < columns; c++) {
+    for (let r = rows - 1; r >= 0; r--) {
       if (board[r][c].color === null) {
+
         // move candies above down
         for (let k = r; k > 0; k--) {
-          board[k][c].color = board[k-1][c].color;
+          board[k][c].color = board[k - 1][c].color;
         }
+
         // top candy gets new random color
         board[0][c].color = random(candyColors);
       }
