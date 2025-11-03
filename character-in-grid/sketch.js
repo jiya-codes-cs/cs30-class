dcbe5ba01c306b157b95f12039e82188c1c47ada
jiya-codes-cs <jiyabangar.official@gmail.com>
2025-11-03 09:32:04 -0600

    Added images to the character in grid demo

--- character-in-grid/sketch.js
+++ character-in-grid/sketch.js
@@ -1,4 +1,4 @@
-// Rectangle Neighbours 2d Array Demo
+// Character in Grid 2d Array Demo
 
 const CELL_SIZE = 50;
 let grid;
@@ -11,7 +11,14 @@ let thePlayer = {
   x: 0,
   y: 0,
 };
+let grassImg;
+let pathImg;
 
+function preload(){
+  grassImg = loadImage("grass.png");
+  pathImg = loadImage("paving.png");
+
+}
 function setup() {
   createCanvas(windowWidth * 0.9, windowHeight * 0.9);
   cols = Math.floor(width/CELL_SIZE);
@@ -90,15 +97,17 @@ function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       if (grid[y][x] === OPEN_TILE) {
-        fill("white");
+        // fill("white");
+        image(pathImg, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
       }
       else if (grid[y][x] === IMPASSIBLE) {
-        fill("black");
+        // fill("black");
+        image(grassImg, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
       }
       else if (grid[y][x] === PLAYER) {
         fill("red");
+        square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
       }
-      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
     }
   }
 }
