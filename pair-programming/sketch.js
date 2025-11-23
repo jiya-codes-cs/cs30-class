4034b38081360f18feaf73e15e436e3998cfd064
jiya-codes-cs <jiyabangar.official@gmail.com>
2025-11-23 11:45:30 -0600

    edited the demo

--- pair-programming/sketch.js
+++ pair-programming/sketch.js
@@ -40,6 +40,8 @@ class Ship {
     this.x = x;
     this.y = y;
     this.image = theImage;
+
+    this.bullets = [];
   }
 
   update() {
@@ -58,17 +60,36 @@ class Ship {
     }
 
     // if doing extra for experts, show bullet(s)
+    for(let i = this.bullets.length - 1; i >= 0; i--) {
+      let bullet = this.bullets[i];
+      bullet.update();
+
+      if (!bullet.isOnScreen()) {
+        this.bullets.splice(i, 1);
+      }
+    }
   }
 
   display() {
     // show the ship
     noStroke();
     image(this.image, this.x, this.y);
+
+    for (let bullet of this.bullets) {
+      bullet.display();
+    }
   }
 
   handleKeyPress() {
     // you only need to use this if you are doing the extra for experts...
     // if you are, you should make a bullet if the space key was pressed
+    if(key === " ") {
+      let bx = this.x + this.image.width / 2 - bulletImage.width / 2;
+      let by = this.y - 20;
+      let bullet = new Bullet(bx,by, 0, -1, bulletImage);
+      this.bullets.push(bullet);
+    }
+
   }
 }
 
@@ -92,16 +113,23 @@ class Bullet {
 
   update() {
     // what does the bullet need to do during each frame? how do we know if it is off screen?
-    this.x = this.dx;
-    this.y = this.dy;
+    this.x += this.dx;
+    this.y += this.dy;
   }
 
   display() {
     // show the bullet
+    image(this.image, this.x, this.y );
   }
 
   isOnScreen() {
     // check if the bullet is still on the screen
+    if(this.x < 0 || this.x > width || this.y < 0 || this.y > height){
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 }
 
